231db113074f293e516acd356c60828f6adb80f6
ionhax-dev <thirukumara.d@zadroit.com>
2025-07-19 10:36:19 +0530

    routes added

--- src/renderer/src/routes/MainAppRoutes.tsx
+++ src/renderer/src/routes/MainAppRoutes.tsx
@@ -1,12 +1,13 @@
 import Login from '@renderer/pages/01-Login/Login'
 import React from 'react'
 
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 const MainAppRoutes: React.FC = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
