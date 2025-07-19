19b021275a9dfc608f71f7ddfc3f50bd22e1c2fc
ionhax-dev <thirukumara.d@zadroit.com>
2025-07-19 12:49:59 +0530

    Header functionality drafted

--- src/renderer/src/routes/MainAppRoutes.tsx
+++ src/renderer/src/routes/MainAppRoutes.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 
 import { Route, Routes } from 'react-router-dom'
 import ForgotPassword from '../pages/01-Login/ForgotPassword'
+import Dashboard from '../pages/02-Dashboard/Dashboard'
+import Settings from '../pages/03-Settings/Settings'
+import Header from '../components/00-Header/Header'
 
 const MainAppRoutes: React.FC = () => {
   return (
@@ -11,6 +14,18 @@ const MainAppRoutes: React.FC = () => {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route
+          path="*"
+          element={
+            <Header>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </Header>
+          }
+        />
       </Routes>
     </div>
   )
